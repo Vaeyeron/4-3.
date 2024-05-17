31d05f3abe02e6798c2096ef0d919d55f3861772
Vaeyeron <bgeorgij14@gmail.com>
2024-05-17 20:41:58 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication1.Desktop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Avalonia;
+using Avalonia.ReactiveUI;
+
+namespace AvaloniaApplication1.Desktop;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
+}
